8a2658e8f230888edc89a2cf691000cc98c22ca2
Nurishka <nurishkav1998@gmail.com>
2019-07-02 19:54:44 +0300

    сдаем мак в сервис

--- SideMenu1/ViewController.swift
+++ SideMenu1/ViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sideMenu: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        sideMenu.target = self.revealViewController()
+        sideMenu.action = Selector("revealToggle:")
     }
+  
 
 
 }
